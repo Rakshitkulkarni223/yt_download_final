76d5b1efd63cb02dac78cb67f8d773090096b6e8
Rakshitkulkarni223 <76918993+Rakshitkulkarni223@users.noreply.github.com>
2022-12-14 15:09:40 +0530

    Your message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
 
 function App() {
+  const [text,setText] = useState("");
+  useEffect(() => {   
+    async function getAllSkills() {
+        const uri = `/api/skills`;
+				const response = await fetch(uri)
+        // console.log(response.text());
+        setText(response.url);
+    }
+    getAllSkills()
+   },[]);
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +20,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {text}
         <a
           className="App-link"
           href="https://reactjs.org"
